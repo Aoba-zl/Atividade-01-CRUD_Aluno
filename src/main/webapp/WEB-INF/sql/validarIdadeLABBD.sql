295706e6f205493178a536352d8b984b9e538280
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-03-25 02:35:48 -0300

    Alteração no tipo de retorno da procedure validarIdadeLABBD. obj: aumentar manutentabilidade e performace

--- src/main/webapp/WEB-INF/sql/validarIdadeLABBD.sql
+++ src/main/webapp/WEB-INF/sql/validarIdadeLABBD.sql
@@ -1,23 +1,19 @@
 -- Validar Idade, idade >= 16
 
-create procedure sp_alunovalidaridade(@dt_nasc date, @saida varchar(100) output)
+-- DROP procedure sp_alunovalidaridade
+
+create procedure sp_alunovalidaridade(@dt_nasc date, @saida BIT output)
 as
     declare @idade int
     set @idade = datediff(year, @dt_nasc, getdate())
+    SET @saida = 0
     if (@idade >= 16)
     begin
-        set @saida = 'Idade valida'
-    end
-    else
-    begin
-        raiserror('Aluno com idade menor que 16 anos', 16, 1)
+        set @saida = 1
     end
 
 
-
-
-
-declare @saida1 varchar(100)
+declare @saida1 BIT
 exec sp_alunovalidaridade '2003-03-15', @saida1 output
 print @saida1
 
