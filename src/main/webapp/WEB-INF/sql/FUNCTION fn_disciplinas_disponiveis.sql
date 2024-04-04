4d5660486f9af605c446978858099f1999bef8e8
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-04-04 20:07:36 -0300

    CORREÇÃO: Ajuste nas disciplinas disponiveis

--- src/main/webapp/WEB-INF/sql/FUNCTION fn_disciplinas_disponiveis.sql
+++ src/main/webapp/WEB-INF/sql/FUNCTION fn_disciplinas_disponiveis.sql
@@ -17,15 +17,14 @@ RETURNS @table TABLE
 BEGIN
     INSERT INTO @table
     SELECT DISTINCT d.codigo, d.nome, d.horas_semanais,
-        CASE WHEN md.estado IS NULL THEN 'disponivel' ELSE 'ja cursou' END AS situacao
-    FROM curso_disciplina cd
-             INNER JOIN disciplina d ON cd.cod_disciplina = d.codigo
-             LEFT JOIN matricula m ON m.cod_curso = cd.cod_curso AND m.ra = @ra
-             LEFT JOIN matricula_disciplina md ON md.cod_disciplina = cd.cod_disciplina AND md.estado NOT LIKE 'matriculado'
-    WHERE m.ra IS NOT NULL AND cd.cod_disciplina NOT IN (
-        SELECT DISTINCT cod_disciplina
-        FROM matricula_disciplina WHERE ra_matricula = @ra AND estado LIKE 'matriculado'
-    )
+                CASE WHEN md.estado IS NULL THEN 'disponivel' ELSE 'ja cursou' END AS situacao
+FROM curso_disciplina cd
+         INNER JOIN disciplina d ON cd.cod_disciplina = d.codigo
+         LEFT JOIN matricula m ON m.cod_curso = cd.cod_curso AND m.ra = @ra
+         LEFT JOIN matricula_disciplina md ON md.cod_disciplina = cd.cod_disciplina
+                AND md.ra_matricula = m.ra
+WHERE m.ra IS NOT NULL AND cd.cod_disciplina NOT IN ( SELECT DISTINCT cod_disciplina
+    FROM matricula_disciplina WHERE ra_matricula = @ra AND estado LIKE 'matriculado')
     RETURN
 END
 
