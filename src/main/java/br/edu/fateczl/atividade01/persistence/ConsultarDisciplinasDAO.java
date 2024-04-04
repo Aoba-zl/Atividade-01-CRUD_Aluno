44087f5adc2d723c7331da0f8c4ae390ab443a23
Luan Camilo Nogueira <luancamilo313@gmail.com>
2024-04-04 18:53:13 -0300

    Tabela sendo populada normalmente

--- src/main/java/br/edu/fateczl/atividade01/persistence/ConsultarDisciplinasDAO.java
+++ src/main/java/br/edu/fateczl/atividade01/persistence/ConsultarDisciplinasDAO.java
@@ -32,7 +32,6 @@ public class ConsultarDisciplinasDAO {
                     where disciplina.codigo = matricula_disciplina.cod_disciplina and
                           horario.id = matricula_disciplina.id_horario and
                           matricula.ra = matricula_disciplina.ra_matricula and
-                          matricula_disciplina.estado = 'matriculado' and
                           matricula.ra = ?
                 """;
         PreparedStatement ps= c.prepareStatement(sql);
